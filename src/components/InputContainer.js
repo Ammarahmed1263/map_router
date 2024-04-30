54c06139d205d32f8ed62ddb0f79a344f193228f
Ammar Ahmed <ammarahmed1263@gmail.com>
2024-04-30 18:10:44 +0300

    define routing structure

--- src/components/InputContainer.js
+++ src/components/InputContainer.js
@@ -1,11 +1,28 @@
-import {View, StyleSheet, StatusBar} from 'react-native';
-import IconTextInput from './IconTextInput';
+import {View, StyleSheet, StatusBar, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import EllipsisDots from './EllipsisDots';
+import AutocompleteSearch from './AutoComplete';
 
-function InputContainer({style}) {
-  return <View style={[styles.container, style]}>
-    <IconTextInput />
-    <IconTextInput />
-  </View>
+function InputContainer({style, colors}) {
+  return (
+    <View style={[styles.container, style]}>
+      <View style={styles.inputContainer}>
+        <Icon name="radio-button-on-outline" color={colors.text} size={25} />
+        <AutocompleteSearch placeholder="Your place" colors={colors} />
+      </View>
+      <View
+        style={styles.separator}>
+        <EllipsisDots count={4} dotSize={13} color={colors.text} />
+        <View
+          style={{...styles.horizontalLine, borderBottomColor: colors.text}}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Icon name="location-outline" color={colors.text} size={25} />
+        <AutocompleteSearch placeholder="Your destination" colors={colors} />
+      </View>
+    </View>
+  );
 }
 
 export default InputContainer;
@@ -14,9 +31,28 @@ const styles = StyleSheet.create({
   container: {
     position: 'absolute',
     alignSelf: 'center',
-    top: StatusBar.currentHeight + 5,
-    backgroundColor: 'blue',
-    width: '92%',
-    borderRadius: 12
-  }
-})
+    height: 113,
+    width: '96%',
+    top: StatusBar.currentHeight + 20,
+    borderRadius: 12,
+    paddingLeft: 10,
+    paddingVertical: 2,
+  },
+  inputContainer: {
+    flex: 2,
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  separator: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+    marginLeft: 10,
+  },
+  horizontalLine: {
+    flex: 1,
+    borderBottomWidth: StyleSheet.hairlineWidth, // Adjust the thickness of the line
+    marginHorizontal: 14,
+  },
+});
